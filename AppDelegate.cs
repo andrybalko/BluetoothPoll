666738df53b5bba5f73a619a48c62c8b43120bfc
andrybalko <arybalko@gmail.com>
2019-05-03 12:50:26 +0300

    update beacon polling

---
+++ AppDelegate.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using BluetoothPoll.ViewModels;
+using CoreBluetooth;
+using CoreLocation;
+using Foundation;
+using Prism;
+using Prism.Ioc;
+using UIKit;
+using UserNotifications;
+using Xamarin.Forms;
+
+namespace BluetoothPoll.Ios
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the
+    // User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
+    [Register("AppDelegate")]
+    public class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+	{
+        private CLLocationManager locationManager;
+
+        // class-level declarations
+
+        List<BeaconItem> beacons;
+
+        public override UIWindow Window
+        {
+            get;
+            set;
+        }
+
+        public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+        {
+            global::Xamarin.Forms.Forms.Init();
+            LoadApplication(new App(new iOSInitializer()));
+
+            if (UIDevice.CurrentDevice.CheckSystemVersion(10, 0))
+            {
+                UNUserNotificationCenter center = UNUserNotificationCenter.Current;
+                center.RequestAuthorization(UNAuthorizationOptions.Alert | UNAuthorizationOptions.Sound, (bool arg1, NSError arg2) =>{});
+                center.Delegate = new NotificationDelegate();
+            }
+            else if (UIDevice.CurrentDevice.CheckSystemVersion(8, 0))
+            {
+                var settings = UIUserNotificationSettings.GetSettingsForTypes(UIUserNotificationType.Alert | UIUserNotificationType.Sound, new NSSet());
+                UIApplication.SharedApplication.RegisterUserNotificationSettings(settings);
+            }
+
+
+            locationManager = new CLLocationManager();
+            var beaconId = new NSUuid(PollViewModel.LOOKUP_UUID);
+            var beaconRegion = new CLBeaconRegion(beaconId, "My Beacon");
+            beaconRegion.NotifyEntryStateOnDisplay = true;
+            beaconRegion.NotifyOnEntry = true;
+
+            locationManager.RegionEntered += (s, e) => {
+
+                if (e.Region.Identifier == "My Beacon")
+                {
+                    Console.WriteLine("Found My Beacon");
+
+                    locationManager.StartRangingBeacons(beaconRegion);
+                    locationManager.DidRangeBeacons += (lm, e2) => {
+                        if (e2.Beacons.Length > 0)
+                        {
+                            foreach (var b in e2.Beacons)
+                            {
+
+                                if (b.Proximity != CLProximity.Unknown)
+                                {
+                                    Console.WriteLine("UUID: {0} | Major: {1} | Minor: {2} | Accuracy: {3} | Proximity: {4} | RSSI: {5}", b.ProximityUuid, b.Major, b.Minor, b.Accuracy, b.Proximity, b.Rssi);
+                                    var exists = false;
+                                    for (int i = 0; i < beacons.Count; i++)
+                                    {
+                                        if (beacons[i].Minor.Equals(b.Minor.ToString()))
+                                        {
+                                            beacons[i].CurrentDistance = Math.Round(b.Accuracy, 2);
+                                            SetProximity(b, beacons[i]);
+                                            exists = true;
+                                        }
+                                    }
+
+                                    if (!exists)
+                                    {
+                                        var newBeacon = new BeaconItem
+                                        {
+                                            Minor = b.Minor.ToString(),
+                                            Name = "",
+                                            CurrentDistance = Math.Round(b.Accuracy, 2)
+                                        };
+                                        SetProximity(b, newBeacon);
+                                        beacons.Add(newBeacon);
+                                    }
+                                }
+                            }
+                        }
+                    };
+                }
+            };
+            locationManager.StartMonitoring(beaconRegion);
+
+           
+			return base.FinishedLaunching(application, launchOptions);
+		}
+
+        void SetProximity(CLBeacon source, BeaconItem dest)
+        {
+
+            CLProximity p = CLProximity.Unknown;
+
+            switch (source.Proximity)
+            {
+                case CLProximity.Immediate:
+                    p = CLProximity.Immediate;
+                    var notification = new UILocalNotification();
+                    notification.FireDate = NSDate.FromTimeIntervalSinceNow(1);
+                    notification.AlertAction = "Device Proximity Alert";
+                    notification.AlertBody = "We are close to required bluetooth device!";
+                    notification.SoundName = UILocalNotification.DefaultSoundName;
+
+                    UIApplication.SharedApplication.ScheduleLocalNotification(notification);
+                    break;
+                case CLProximity.Near:
+                    p = CLProximity.Near;
+                    break;
+                case CLProximity.Far:
+                    p = CLProximity.Far;
+                    break;
+            }
+
+
+            dest.Proximity = p;
+        }
+
+        public override void ReceivedLocalNotification(UIApplication application, UILocalNotification notification)
+        {
+            // show an alert
+            UIAlertController okayAlertController = UIAlertController.Create(notification.AlertAction, notification.AlertBody, UIAlertControllerStyle.Alert);
+            okayAlertController.AddAction(UIAlertAction.Create("OK", UIAlertActionStyle.Default, null));
+            UIApplication.SharedApplication.KeyWindow.RootViewController.PresentViewController(okayAlertController, true, null);
+        }
+    }
+
+	public class iOSInitializer : IPlatformInitializer
+	{
+		public void RegisterTypes(IContainerRegistry containerRegistry)
+		{
+		}
+	}
+
+    public class NotificationDelegate : UNUserNotificationCenterDelegate
+    {
+        public override void WillPresentNotification(UNUserNotificationCenter center, UNNotification notification, Action<UNNotificationPresentationOptions> completionHandler)
+        {
+            completionHandler(UNNotificationPresentationOptions.Alert | UNNotificationPresentationOptions.Sound);
+        }
+
+    }
+
+    public class BeaconItem
+    {
+        public string Minor { get; set; }
+        public string Name { get; set; }
+        public double CurrentDistance { get; set; }
+        public CLProximity Proximity { get; set; }
+    }
+}
+
---
+++ PollBtDevices.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BluetoothPoll.Ios.Services;
+using BluetoothPoll.Models;
+using BluetoothPoll.Services;
+using BluetoothPoll.ViewModels;
+using CoreBluetooth;
+using CoreFoundation;
+using ExternalAccessory;
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(PollBtDevices))]
+namespace BluetoothPoll.Ios.Services
+{
+	public class PollBtDevices:IPollBtDevices
+	{
+		private readonly CBCentralManager manager = new CBCentralManager(new CbCentralDelegate(), DispatchQueue.DefaultGlobalQueue,
+                                                                         new CBCentralInitOptions { ShowPowerAlert = true });
+
+		
+		public async Task<bool> Search()
+		{
+			EAAccessoryManager mgr = EAAccessoryManager.SharedAccessoryManager;
+			var accessories = mgr.ConnectedAccessories;
+			foreach (var accessory in accessories)
+			{
+				Console.WriteLine(accessory.ToString());
+			}
+
+			if (manager.State == CBCentralManagerState.PoweredOn)
+			{
+                await ScanPeripherals();
+				return await Task.FromResult(false);
+			}
+			return await Task.FromResult(true);
+		}
+
+        private async Task ScanPeripherals()
+        {
+            if (manager.State == CBCentralManagerState.PoweredOn)
+            {
+                manager.ScanForPeripherals(new CBUUID[0]);
+                await Task.Delay(15000);
+                manager.StopScan();
+            }
+        }
+    }
+
+	public class CbCentralDelegate : CBCentralManagerDelegate
+	{
+		public override void UpdatedState(CBCentralManager central)
+		{
+			if (central.State == CBCentralManagerState.PoweredOn)
+			{
+			}
+			else
+			{
+				//points user to turn bluetooth on
+				Device.BeginInvokeOnMainThread(() =>
+				{
+					if (UIDevice.CurrentDevice.CheckSystemVersion(9, 0))
+					{
+						UIApplication.SharedApplication.OpenUrl(new NSUrl("prefs:root=Bluetooth"));
+					}
+					else if (UIDevice.CurrentDevice.CheckSystemVersion(8, 0))
+					{
+						UIApplication.SharedApplication.OpenUrl(new NSUrl("prefs:root=General&path=Bluetooth"));
+					}
+					else
+					{
+						UIApplication.SharedApplication.OpenUrl(new NSUrl("App-Prefs:root=Bluetooth"));
+					}
+				});
+
+			}
+		}
+
+        
+        public override void DiscoveredPeripheral(CBCentralManager central, CBPeripheral peripheral, NSDictionary advertisementData, NSNumber RSSI)
+        {
+            if (peripheral.Identifier.AsString() == PollViewModel.LOOKUP_UUID)
+            {
+                //show local notification
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    if (UIDevice.CurrentDevice.CheckSystemVersion(10, 0))
+                    {
+
+                    }
+                    else if (UIDevice.CurrentDevice.CheckSystemVersion(8, 0))
+                    {
+
+                    }
+                });
+            }
+            MessagingCenter.Send<App, BtDevice>((App)Xamarin.Forms.Application.Current, BtDevice.NewDevice,
+                new BtDevice() { Mac = "-", Name = peripheral.Name });
+        }
+	}
+}
